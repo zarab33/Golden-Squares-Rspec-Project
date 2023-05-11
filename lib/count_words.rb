f46f5e2fa9e235491ca73fd79cdb9f4efdac0104
Zara <zarabucknor@yahoo.co.uk>
2023-05-11 15:32:35 +0100

    Initial Test passed

---
+++ lib/count_words.rb
@@ -0,0 +1,5 @@
+def  count_words(str)
+    str_arr = str.split(" ")
+    count = str_arr.length
+    "Number of words in string: #{count}"
+end
--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,3 +1,9 @@
 def make_snippet(word)
+  split_word = word.split(" ")
+  if split_word.length <= 5
   return word
+  else 
+    result = split_word[0,5].join(" ")
+    return result + " ..."
+  end
 end
