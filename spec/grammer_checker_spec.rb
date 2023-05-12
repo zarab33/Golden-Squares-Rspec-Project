a06263008477e3242bdb7666e0ee9d8e68d95123
Zara <zarabucknor@yahoo.co.uk>
2023-05-13 00:48:42 +0100

    added rspec test for exclamation marks

--- spec/grammer_checker_spec.rb
+++ spec/grammer_checker_spec.rb
@@ -18,4 +18,32 @@ RSpec.describe "grammer_checker" do
         expect(result).to eq false
     end
    end
+
+   context "Given a sentence without a capital or fullstop" do
+    it "returns false" do 
+        result = grammer_checker("hello, coders i make mistakes") 
+        expect(result).to eq false
+    end
+   end
+
+   context "Given a sentence with a capital word and with fullstop" do
+    it "returns true" do 
+        result = grammer_checker("HELLO, coders i can use correct grammer.")  
+        expect(result).to eq true
+    end
+   end
+
+   context "Given a sentence with a capital letter and question mark" do
+    it "returns true" do 
+        result = grammer_checker("Hello, coders i can use correct grammer?")  
+        expect(result).to eq true
+    end
+   end
+
+   context "Given a sentence with a capital letter and exclamation mark" do
+    it "returns true" do 
+        result = grammer_checker("Hello, coders i can use correct grammer!")  
+        expect(result).to eq true
+    end
+   end
 end
