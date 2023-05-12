63b9a04fe970d52c020e3314b360e8d93d221ae4
Zara <zarabucknor@yahoo.co.uk>
2023-05-12 19:18:39 +0100

    Refactored and Passed final RSpec test

--- lib/estimate_reading_time.rb
+++ lib/estimate_reading_time.rb
@@ -1,8 +1,4 @@
 def estimate_reading_time(text)
     words = text.split(" ")
-   if words.length == 0
-    return 0
-   elsif words.length < 200
-    return 1
-   end
+   return (words.length / 200.to_f).ceil
 end
--- spec/estimate_reading_time_spec.rb
+++ spec/estimate_reading_time_spec.rb
@@ -7,13 +7,28 @@ RSpec.describe "estimate_reading_time" do
     end
 
     it "estimates a result for a text under 200 words return 1" do 
-        result = estimate_reading_time("one")
+        result = estimate_reading_time("one ")
         expect(result).to eq 1
     end 
 
     it "estimates a result for a text of 200 words return 1" do 
-        result = estimate_reading_time("Two" * 200)
+        result = estimate_reading_time("Two " * 200)
         expect(result).to eq 1
+    end 
+
+    it "estimates a result for a text of 300 words return 2" do 
+        result = estimate_reading_time("Three " * 300)
+        expect(result).to eq 2
+    end 
+
+    it "estimates a result for a text of 400 words return 2" do 
+        result = estimate_reading_time("Four " * 400)
+        expect(result).to eq 2
+    end 
+
+    it "estimates a result for a text of 20,000 words return 100" do 
+        result = estimate_reading_time("Twenty_thousand " * 20000)
+        expect(result).to eq 100
     end 
 end
 
