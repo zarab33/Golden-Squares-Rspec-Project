e7b9f13c19ef152ca6763ec04497067352916b33
Zara <zarabucknor@yahoo.co.uk>
2023-05-13 00:44:31 +0100

    changed variable to (sentence_end) to include more punctuations

--- lib/grammer_checker.rb
+++ lib/grammer_checker.rb
@@ -1,11 +1,8 @@
 def grammer_checker(text)
     fail "Not a sentence." if text.empty?
+    
     first_capital_letter = text[0].upcase == text[0]
-    full_stop_str_end = text[-1] == "."
-    if first_capital_letter && full_stop_str_end
-     return true
-  else
-     return false
-        
-    end
+    sentence_end = ['.', '?', '!'].include?(text[-1])
+
+     return first_capital_letter && sentence_end
 end
