329bb4156899f39f7dbcdd34f789ae9403447483
Zara <zarabucknor@yahoo.co.uk>
2023-05-12 19:19:55 +0100

    refactored method so easily readable

--- lib/estimate_reading_time.rb
+++ lib/estimate_reading_time.rb
@@ -1,4 +1,6 @@
 def estimate_reading_time(text)
-    words = text.split(" ")
-   return (words.length / 200.to_f).ceil
-end
+    words_per_minute = 200
+    words_count = text.split.size
+    (words_count.to_f / words_per_minute).ceil
+  end
+  
