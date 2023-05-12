8f6ef46c108c864f9fe9b41b605235ac02051cfc
Zara <zarabucknor@yahoo.co.uk>
2023-05-12 17:19:15 +0100

    initial tests passed

--- spec/estimate_reading_time_spec.rb
+++ spec/estimate_reading_time_spec.rb
@@ -1,9 +1,9 @@
 require 'estimate_reading_time'
 
-RSpec.describe estimate_reading_time do
+RSpec.describe "estimate_reading_time" do
     it "empty string returns zero" do 
         result = estimate_reading_time("")
-        expect (result).to eq 0
+        expect(result).to eq 0
     end
 end
 
