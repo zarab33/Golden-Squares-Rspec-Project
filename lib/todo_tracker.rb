eead412486b5579491ff858aa4df4ccb97438901
Zara <zarabucknor@yahoo.co.uk>
2023-05-15 15:06:26 +0100

    first test passed, return a empty string fail

--- lib/todo_tracker.rb
+++ lib/todo_tracker.rb
@@ -2,9 +2,4 @@ def todo_tracker(text)
     if text.empty?
         fail "No text found"
     end
-        if text.include? ("#TODO")
-            return true
-        else
-            return false
-    end
 end
--- spec/todo_tracker_spec.rb
+++ spec/todo_tracker_spec.rb
@@ -1,24 +1,8 @@
 require 'todo_tracker'
 
-RSpec.describe "todo_tracker" do 
-    context "Empty string returns an error" do
-    it "fails" do
-        expect{ todo_tracker ("")}.to raise_error("No text found")
-    end
+RSpec.describe "todo_tracker" do
+    context "empty sting return an error"
+    it "fail" do
+    expect{todo_tracker("")}.to raise_error("No text found")
 end
-
-    context "Given a text with the word #TODO" do 
-    it "returns true" do
-        result = todo_tracker("The text includes #TODO")
-        expect(result).to eq true
-    end
-end
-
-context "Given a text without the word #TODO" do 
-    it "returns false" do
-        result = todo_tracker("The text dosent include keyword")
-        expect(result).to eq false
-    end
-end
-
 end
