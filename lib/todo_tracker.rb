f5a5193edfd14618cc8973cad2d66fe3f7fafa60
Zara <zarabucknor@yahoo.co.uk>
2023-05-15 15:11:25 +0100

    test passed true if the word #TODO found

--- lib/todo_tracker.rb
+++ lib/todo_tracker.rb
@@ -2,4 +2,9 @@ def todo_tracker(text)
     if text.empty?
         fail "No text found"
     end
+    if text.include? "#TODO"
+        return true
+    else
+        return false
+    end
 end
--- spec/todo_tracker_spec.rb
+++ spec/todo_tracker_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe "todo_tracker" do
     it "fail" do
     expect{todo_tracker("")}.to raise_error("No text found")
 end
+
+    context "Given a text with the word #TODO" do
+        it "returns true" do
+            result = todo_tracker("The text includes #TODO")
+            expect(result).to eq true
+        end
+    end
+
 end
