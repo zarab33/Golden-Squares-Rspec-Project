49e90ab8787fa1b4dc70b1c40a9b25284a70e4c1
Zara <zarabucknor@yahoo.co.uk>
2023-05-15 14:44:45 +0100

    passed rspec given a strind with #TODO

--- spec/todo_tracker_spec.rb
+++ spec/todo_tracker_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe "todo_tracker" do
         expect{ todo_tracker ("")}.to raise_error("No text found")
     end
 end
+
+    context "Given a text with the word #TODO" do 
+    it "returns true" do
+        result = todo_tracker("The text includes #TODO")
+        expect(result).to eq true
+    end
+end
 end
