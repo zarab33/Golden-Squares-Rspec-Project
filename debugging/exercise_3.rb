cf74fec9cafd0796d4e44c95b4a62a884cfc98f5
Zara <zarabucknor@yahoo.co.uk>
2023-05-16 11:36:05 +0100

    Error found in line 2 + 10 (a..z) also line 12, code fixed!

--- debugging/exercise_3.rb
+++ debugging/exercise_3.rb
@@ -1,5 +1,5 @@
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
@@ -7,13 +7,18 @@ def encode(plaintext, key)
 end
 
 def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    cipher[ char.ord - 65]
   end
   return plaintext_chars.join
 end
 
+p encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+p "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+
+p decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+p "theswiftfoxjumpedoverthelazydog"
 # Intended output:
 #
 # > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
