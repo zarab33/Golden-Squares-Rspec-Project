0a1c0995b69ab77db95e49b3ac9cd1cbd49fb4b9
Zara <zarabucknor@yahoo.co.uk>
2023-05-17 15:02:02 +0100

    passed test on reading time method

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -19,4 +19,14 @@ RSpec.describe DiaryEntry do
             expect(diary_entry.count_words).to eq 0
         
     end
+
+    describe "#reading_time" do
+        context "given a wpm of some sensible number (200)" do
+            it "returns the ceiling of the number of minutes it takes to read the context" do
+            diary_entry = DiaryEntry.new("my_title", "one " * 550)
+            expect(diary_entry.reading_time(200)).to eq 3
+            end
+        end
+    end
+
 end
