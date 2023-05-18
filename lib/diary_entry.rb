f72363f9a105004334e8a607229814e06ab5a290
Zara <zarabucknor@yahoo.co.uk>
2023-05-18 13:19:29 +0100

    restarts after reading the whole contects.

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -35,7 +35,11 @@ class DiaryEntry
     start_from = @furthest_word_read
     end_at = @furthest_word_read + no_words_we_can_read
     word_list = words[start_from, end_at]
+    if end_at > count_words
+      @furthest_word_read = 0
+    else
     @furthest_word_read = end_at
+    end
     return word_list.join(" ")
     
     # `wpm` is an integer representing the number
