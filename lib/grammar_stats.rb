d8b7d5f14d16d33acac546ae6c1d9e5e5166ed45
Zara <zarabucknor@yahoo.co.uk>
2023-05-18 15:05:37 +0100

    Returns as an integer the percentage of texts checked so far that passed

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,23 +1,42 @@
 class GrammarStats
   def initialize
     @check = true
+    @pass_count = 0
+    @total_count = 0
     # ...
   end
 
   def check(text) 
      fail "Not a sentence." if text.empty?
-     
+
     first_capital_letter = text[0].upcase == text[0]
     sentence_end = ['.', '?', '!'].include?(text[-1])
 
-     return first_capital_letter && sentence_end
+     result = first_capital_letter && sentence_end
+
+      if result
+        @pass_count += 1
+      end
+        @total_count += 1
+        result 
+    end
+
+
     # text is a string
     # Returns true or false depending on whether the text begins with a capital
     # letter and ends with a sentence-ending punctuation mark.
-  end
+    
 
   def percentage_good
+      if @total_count == 0
+        return 0.0
+      else
+        return (@pass_count.to_f / @total_count.to_f) * 100.0
+      end
+    end
+  end
+    
+
+
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
-  end
-end
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -4,13 +4,25 @@ RSpec.describe GrammarStats do
     describe "#check" do
         it "returns true once it checks the letters" do
             stats = GrammarStats.new 
-            expect(stats.check("Hello world.")).to eq true
+            expect(stats.check("Howdy people.")).to eq true
         end
-    end
+    
 
         it "empty string returns it fails" do 
             stats = GrammarStats.new 
             expect{ stats.check("") }.to raise_error "Not a sentence."
         end
-    
+    end
+
+     describe "#percentage_good" do
+        it "Returns as an integer the percentage of texts checked so far that passed" do
+            stats = GrammarStats.new 
+            stats.check("Howdy people.")
+            stats.check("Howdy people!")
+            stats.check("Howdy people?")
+            stats.check("Howdy people")
+            stats.check("howdy people")
+            expect(stats.percentage_good).to eq 60
+        end
+    end
 end
