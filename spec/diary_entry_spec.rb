71521a8d9abdd05ffa0ce6b4bb81222b0f9559c7
Zara <zarabucknor@yahoo.co.uk>
2023-05-18 13:17:00 +0100

    restarts after reading the whole contects.

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -6,61 +6,69 @@ RSpec.describe DiaryEntry do
         expect(diary_entry.title).to eq "my_title"
         expect(diary_entry.contents).to eq "my_contents"
     end
-
     describe "#count_words" do
         it "returns the number of words in a string" do
-            diary_entry = DiaryEntry.new("my_title", "some contents here")
-            expect(diary_entry.count_words).to eq 3
+        diary_entry = DiaryEntry.new("my_title", "some contents here")
+        expect(diary_entry.count_words).to eq 3
+        end
+        it "returns zero when it has an empty string" do
+        diary_entry = DiaryEntry.new("my_title", "")
+        expect(diary_entry.count_words).to eq 0
         end
     end
-
-    it "returns zero when contents is empty" do
-           diary_entry = DiaryEntry.new("my_title", "")
-            expect(diary_entry.count_words).to eq 0
-        
-    end
-
     describe "#reading_time" do
         context "given a wpm of some sensible number (200)" do
-            it "returns the ceiling of the number of minutes it takes to read the context" do
+            it "returns the ceiling of the number of minutes it takes to read the contents." do
             diary_entry = DiaryEntry.new("my_title", "one " * 550)
             expect(diary_entry.reading_time(200)).to eq 3
             end
         end
-    end
-
-    context "given a wpm of 0" do
+        context "given a wpm of zero" do
             it "fails" do
-            diary_entry = DiaryEntry.new("my_title", "one two three ")
-            expect { diary_entry.reading_time(0) }.to raise_error "Fail! Reading time must be above zero."
-            end
-        
-    end
-
-    describe "#reading_chunk" do
-        context "with a contents readable within the given minutes" do
-            it "returns the full contents" do
             diary_entry = DiaryEntry.new("my_title", "one two three")
-            chunk = diary_entry.reading_chunk(200, 1)
-            expect(chunk).to eq "one two three"
+            expect{ diary_entry.reading_time(0) }.to raise_error "Fail! Reading time must be above zero." 
             end
         end
     end
-
-    context "with a contents unreadable within the given time" do
-            it "returns a readable chunk" do
+    describe "#reading_chunk" do
+        context "With a contents readable within the time" do
+            it "returns the full contents." do
             diary_entry = DiaryEntry.new("my_title", "one two three")
-            chunk = diary_entry.reading_chunk(2, 1)
-            expect(chunk).to eq "one two"
+            expect(diary_entry.reading_chunk(200, 1)).to eq "one two three"
             end
-        
+        end
+        context "With a contents readable within the time" do
+            it "returns the readable chunk." do
+            diary_entry = DiaryEntry.new("my_title", "one two three")
+            expect(diary_entry.reading_chunk(2, 1)).to eq "one two"
+            end
+            it "returns the readable chunk next time we are asked." do
+                diary_entry = DiaryEntry.new("my_title", "one two three")
+                diary_entry.reading_chunk(2, 1)
+                expect(diary_entry.reading_chunk(2, 1)).to eq "three"
+            end
+            #now it calls the previous two diary_entry to work through & get to the end
+            it "restarts after reading the whole contects." do
+                diary_entry = DiaryEntry.new("my_title", "one two three")
+                diary_entry.reading_chunk(2, 1)
+                diary_entry.reading_chunk(2, 1)
+                expect(diary_entry.reading_chunk(2, 1)).to eq "one two"
+            end
+        end
     end
-
-      it "returns the next chunk, next time we are asked" do
-            diary_entry = DiaryEntry.new("my_title", "one two three")
-            diary_entry.reading_chunk(2, 1)
-            chunk = diary_entry.reading_chunk(2, 1)
-            expect(chunk).to eq "three"
-            end
-    
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
