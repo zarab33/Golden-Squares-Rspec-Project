e7084ac16c31d7eeb762dbda0128e93953b2fd40
Zara <zarabucknor@yahoo.co.uk>
2023-05-19 10:38:57 +0100

    Added the the design template

--- lib/todo_list_class.rb
+++ lib/todo_list_class.rb
@@ -1,2 +1,25 @@
 class TodoList
+
+  def initialize
+    #@task = []
+
+  end
+
+  def add_task(task)
+    #@task << task
+
+  end
+
+  def view_task
+    return @task
+
+  end
+
+  def mark_task_as_complete(task)
+    is_completed = false
+
+    @task.delete_at()
+
+  end
+
 end
