d6557b40f656e09c7c8f502dd40d33dbb3d53a70
Zara <zarabucknor@yahoo.co.uk>
2023-05-21 00:39:01 +0100

    Starts with an empty task list.

--- 8_Test_Drive_a_Class/spec/todo_list_spec.rb
+++ 8_Test_Drive_a_Class/spec/todo_list_spec.rb
@@ -1,1 +1,15 @@
 require 'todo_list'
+
+RSpec.describe TodoList do
+    context "Starts off with an empty task list" do
+        it "has an empty list of tasks" do
+            todo = TodoList.new
+            expect(todo.incomplete).to eq []
+        end
+
+        it "has an empty list of completed tasks" do
+            todo = TodoList.new
+            expect(todo.incomplete).to eq []
+        end
+    end
+end
