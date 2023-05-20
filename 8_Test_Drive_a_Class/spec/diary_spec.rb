a75857e68198f54c53a97cce2f6d0e2204b6b448
Zara <zarabucknor@yahoo.co.uk>
2023-05-20 17:36:47 +0100

    Test for word count of zerc to return 0

--- 8_Test_Drive_a_Class/spec/diary_spec.rb
+++ 8_Test_Drive_a_Class/spec/diary_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe Diary do
         diary = Diary.new
         expect(diary.all).to eq []
     end
+    it "has a word count of 0" do
+        diary = Diary.new
+        expect(diary.count_words).to eq 0
 end
 
