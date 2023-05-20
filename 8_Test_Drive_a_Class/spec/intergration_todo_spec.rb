0a8289eb30d972aebdba58fb3de0df1cb83af2bb
Zara <zarabucknor@yahoo.co.uk>
2023-05-20 23:04:35 +0100

    Adds a task and returns it to the incomplete list.

--- 8_Test_Drive_a_Class/spec/intergration_todo_spec.rb
+++ 8_Test_Drive_a_Class/spec/intergration_todo_spec.rb
@@ -1,2 +1,14 @@
-require 'todo'
 require 'todo_list'
+require 'todo'
+
+
+RSpec.describe "intergration" do
+    context "adds a task" do
+        it "to be completed" do
+            todos = TodoList.new
+            tasks = Todo.new("Wash the windows")
+            todos.add(tasks)
+            expect(todos.incomplete).to eq [tasks]
+        end
+    end
+end
