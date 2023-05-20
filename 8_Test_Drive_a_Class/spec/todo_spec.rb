a16c1d36a22446fb93552a1bbf4f0f608050e47e
Zara <zarabucknor@yahoo.co.uk>
2023-05-20 23:11:33 +0100

    given no tasks it returns an empty string

--- 8_Test_Drive_a_Class/spec/todo_spec.rb
+++ 8_Test_Drive_a_Class/spec/todo_spec.rb
@@ -1,7 +1,14 @@
 require 'todo'
 
 RSpec.describe Todo do
-    context "given a tasks" do 
+    context "given no tasks" do 
+        it "returns an empty string" do
+            tasks = Todo.new("")
+            expect(tasks.task).to eq ""
+        end
+    end
+
+    context "given no tasks" do 
         it "returns a task" do
             tasks = Todo.new("Wash the windows")
             expect(tasks.task).to eq "Wash the windows"
