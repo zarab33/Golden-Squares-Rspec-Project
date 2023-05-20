65cf82195fcdaae2937649b797903868e6281226
Zara <zarabucknor@yahoo.co.uk>
2023-05-20 17:34:49 +0100

    Counts all the words in diary entries contents.

--- 8_Test_Drive_a_Class/spec/intergration_spec.rb
+++ 8_Test_Drive_a_Class/spec/intergration_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe "integration" do
             diary.add(diary_entry_2)
             expect(diary.all).to eq [diary_entry_1, diary_entry_2]
         end
+        
+        describe "#count_words" do
+         it "it counts all the words in diary entries contents" do
+            diary = Diary.new
+            diary_entry_1 = DiaryEntry.new("my title", "my contents")
+            diary_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
+            diary.add(diary_entry_1)
+            diary.add(diary_entry_2)
+            expect(diary.count_words).to eq 5
+         end
+        end
     end
 end
