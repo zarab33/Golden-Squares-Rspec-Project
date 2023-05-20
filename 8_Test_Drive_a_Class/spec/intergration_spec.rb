64471a2b60d11cad6d79eab89fc73f25702a9cd7
Zara <zarabucknor@yahoo.co.uk>
2023-05-20 18:41:46 +0100

    fails if the wpm is zero

--- 8_Test_Drive_a_Class/spec/intergration_spec.rb
+++ 8_Test_Drive_a_Class/spec/intergration_spec.rb
@@ -24,7 +24,14 @@ RSpec.describe "integration" do
     end
 
     describe "#reading time" do
-         it "it calculates the reading time for all entries" do
+        it "fails if the wpm is zero" do
+            diary = Diary.new
+            diary_entry_1 = DiaryEntry.new("my title", "my contents 1")
+            diary.add(diary_entry_1)
+            expect { diary.reading_time(0) }.to raise_error ("WPM must be positive")
+        end
+
+        it "it calculates the reading time for all entries" do
             diary = Diary.new
             diary_entry_1 = DiaryEntry.new("my title", "my contents")
             diary_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
