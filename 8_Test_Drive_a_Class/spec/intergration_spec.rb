a4d3d7be363dfa3bced461af5145c9bd2b31d215
Zara <zarabucknor@yahoo.co.uk>
2023-05-20 17:06:38 +0100

    Returns a list of entries added

---
+++ 8_Test_Drive_a_Class/spec/intergration_spec.rb
@@ -0,0 +1,15 @@
+require 'diary'
+require 'diary_entry'
+
+RSpec.describe "integration" do
+    context "after adding some entries" do
+        it "Returns a list of entries added" do
+            diary = Diary.new
+            diary_entry_1 = DiaryEntry.new("my title", "my contents")
+            diary_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
+            diary.add(diary_entry_1)
+            diary.add(diary_entry_2)
+            expect(diary.all).to eq [diary_entry_1, diary_entry_2]
+        end
+    end
+end
