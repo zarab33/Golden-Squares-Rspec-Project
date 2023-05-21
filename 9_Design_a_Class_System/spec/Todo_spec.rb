8b2c1de0996034b9d10918d51cdc1d0ae90d7a5a
Zara <zarabucknor@yahoo.co.uk>
2023-05-21 23:21:09 +0100

    Todo tasks added to diary entry.

--- 9_Design_a_Class_System/spec/Todo_spec.rb
+++ 9_Design_a_Class_System/spec/Todo_spec.rb
@@ -1,20 +1,41 @@
-=begin
+require 'Todo'
 
-#1.      "given a task"
-        "returns a task"
-        tasks = Todo.new("Wash the winws")
-        (tasks.task) => "Wash the winws"
+RSpec.describe Todo do
+    context "given a task" do 
+        it "returns a task" do
+            tasks = Todo.new("Wash the windows")
+            expect(tasks.task).to eq "Wash the windows"
+        end
+    end
+   
+    it "given no tasks has an empty list" do
+        tasks = Todo.new([])
+        expect(tasks.task).to eq []
+    end
+
+    it "when given a task, puts the task in list" do
+        todo = Todo.new("Do the dishes")
+        expect(todo.task).to eq "Do the dishes"
+    end
 
 
-#2.     "task is not complete initially"
-        tasks = Todo.new("Wash the winws")
-        (tasks.done?) =>  false
+    it "when given two tasks they are both added to the list" do
+        todo = Todo.new("Do the dishes")
+        todo = Todo.new("Clean the car")
+        expect(todo.task).to eq ("Do the dishes", "Clean the car")
+    end
 
-
- #3.    "task can be marked ne when complete"
-        tasks = Todo.new("Wash the winws")
-        tasks.mark_done!
-        (tasks.done?) =>  true
-
-        
-=end
+    xit "when given a task to complete it removes it from the list" do
+        todo_list = Todo.new
+        todo_list.add("Do the ironing")
+        todo_list.add("Go walk the dog")
+        todo_list.complete("Do the ironing")
+        expect(todo_list.task).to eq ["Go walk the dog"]
+    end
+   
+    xit "fails when asked to complete a task that is not on the list" do
+        todo_list = Todo.new
+        todo_list.add("Cook dinner") 
+        expect{ todo_list.complete("Go walk the dog") }.to raise_error "No such task."
+    end
+end
