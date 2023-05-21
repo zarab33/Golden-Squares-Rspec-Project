fdeff4e8e5a934efb384408aa97526c5e5c23050
Zara <zarabucknor@yahoo.co.uk>
2023-05-21 20:06:04 +0100

    Spelling error

--- 9_Design_a_Class_System/spec/DiaryEntry_spec.rb
+++ 9_Design_a_Class_System/spec/DiaryEntry_spec.rb
@@ -19,29 +19,5 @@ RSpec.describe DiaryEntry do
             expect(diary_entry.count_words).to eq 8
         end
     end
-end    
-=begin        describe "#reading_time" do
-            it "fails if the wpm is zero" do
-                diary_entry = DiaryEntry.new("my title", "Hi Giraffe!")
-                expect { diary_entry.reading_time(0) }.to raise_error "WPM must be positive"
-            end
+end   
 
-            it "returns zero if empty contents" do
-                diary_entry = DiaryEntry.new("my title", "")
-                expect(diary_entry.reading_time(2)).to eq 0
-            end
-
-            it "returns one if contents is one word" do
-                diary_entry = DiaryEntry.new("my title", "Coder")
-                expect(diary_entry.reading_time(2)).to eq 1
-            end
-
-            it "returns a reading time for the contents" do
-                diary_entry = DiaryEntry.new("my title", "Happy days are on friday!")
-                expect(diary_entry.reading_time(2)).to eq 3
-            end
-        end    
-    end
-
-end
-=end
